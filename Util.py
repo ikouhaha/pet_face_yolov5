de68458b110a62d85384e4d2d33e2598518f4b2e
Dennis Au Yeung <ikouhaha888@gmail.com>
2022-02-02 17:10:46 +0800

    update

---
+++ Util.py
@@ -0,0 +1,29 @@
+
+from package import *
+
+if(platform.node()=="LAPTOP-MEFC1PDG"):
+    xmlfilepath = "./data/Annotations"
+    txtsavepath = "./data/ImageSets"
+    # dataset_path = "./dataset"
+    # model_path = "./models"
+    # result_path = "./result"
+    # sample_path = "./samples"
+    # logs_path = "./logs" 
+    # lmk_logs_path = "./lmk_logs"
+    # images_path = "./images" 
+else:
+    xmlfilepath = "./source/cats"
+    txtsavepath = "./data/ImageSets"
+    # dataset_path = "./dataset"
+    # model_path = "/content/drive/MyDrive/PET_FACE/models"
+    # result_path = "/content/drive/MyDrive/PET_FACE/result"
+    # sample_path = "/content/drive/MyDrive/PET_FACE/samples"
+    # logs_path = "/content/drive/MyDrive/PET_FACE/logs"
+    # lmk_logs_path = "/content/drive/MyDrive/PET_FACE/lmk_logs" 
+    # images_path = "/content/drive/MyDrive/PET_FACE/images" 
+
+# shutil.rmtree(logs_path,ignore_errors=True)
+# shutil.rmtree(lmk_logs_path,ignore_errors=True)
+
+Path(xmlfilepath).mkdir(parents=True, exist_ok=True)
+Path(txtsavepath).mkdir(parents=True, exist_ok=True)
---
+++ package.py
@@ -0,0 +1,33 @@
+import random
+import cv2, os
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import datetime
+from pathlib import Path
+import datetime
+import sys
+import tensorflow as tf
+import platform
+import math
+
+
+import tensorflow.keras as keras
+import tensorflow as tf
+import shutil
+
+import datetime
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.applications import mobilenet_v2
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from keras.models import Model, load_model
+from math import atan2, degrees
+# from minetorch.miner import Miner
+# from minetorch.metrics import MultiClassesClassificationMetricWithLogic
+# from minetorch.plugins.noise_detector import NoiseSampleDetector
+# from minetorch.spreadsheet import GoogleSheet
+# from torchvision import datasets, transforms
+#from minetorch.spreadsheet.GoogleSheet
