cdb960023879e2871394226fc06cbc2b7cc62507
Dennis Au Yeung <ikouhaha888@gmail.com>
2022-02-02 19:18:25 +0800

    update path

--- Util.py
+++ Util.py
@@ -1,35 +1,24 @@
 
 from package import *
 
-if(platform.node()=="LAPTOP-MEFC1PDG"):
-    xmlfilepath = "./data/Annotations"
-    txtsavepath = "./data/ImageSets"
-    labelPath = "./data/labels"
-    imgPath = "./data/images"
-    # dataset_path = "./dataset"
-    # model_path = "./models"
-    # result_path = "./result"
-    # sample_path = "./samples"
-    # logs_path = "./logs" 
-    # lmk_logs_path = "./lmk_logs"
-    # images_path = "./images" 
-else:
-    xmlfilepath = "/content/drive/MyDrive/pet_face_yolo/data/Annotations"
-    txtsavepath = "./data/ImageSets"
-    labelPath = "./data/labels"
-    imgPath = "/content/drive/MyDrive/pet_face_yolo/data/images"
-    # dataset_path = "./dataset"
-    # model_path = "/content/drive/MyDrive/PET_FACE/models"
-    # result_path = "/content/drive/MyDrive/PET_FACE/result"
-    # sample_path = "/content/drive/MyDrive/PET_FACE/samples"
-    # logs_path = "/content/drive/MyDrive/PET_FACE/logs"
-    # lmk_logs_path = "/content/drive/MyDrive/PET_FACE/lmk_logs" 
-    # images_path = "/content/drive/MyDrive/PET_FACE/images" 
+googlePath = "/content/drive/MyDrive/pet_face_yolo/data"
 
-# shutil.rmtree(logs_path,ignore_errors=True)
-# shutil.rmtree(lmk_logs_path,ignore_errors=True)
 
-Path(xmlfilepath).mkdir(parents=True, exist_ok=True)
-Path(txtsavepath).mkdir(parents=True, exist_ok=True)
-Path(labelPath).mkdir(parents=True, exist_ok=True)
-Path(imgPath).mkdir(parents=True, exist_ok=True)
+xmlfilepath = "data/Annotations"
+txtsavepath = "data/ImageSets"
+labelPath = "data/labels"
+imgPath = "data/images"
+
+
+
+def syncFile():
+    Path(xmlfilepath).mkdir(parents=True, exist_ok=True)
+    Path(txtsavepath).mkdir(parents=True, exist_ok=True)
+    Path(labelPath).mkdir(parents=True, exist_ok=True)
+    Path(imgPath).mkdir(parents=True, exist_ok=True)
+
+    if(os.path.exists(googlePath)):
+        shutil.rmtree(xmlfilepath,ignore_errors=True)
+        shutil.rmtree(imgPath,ignore_errors=True)
+        shutil.copy(googlePath+"/Annotations", xmlfilepath)
+        shutil.copy(googlePath+"/images", imgPath)
