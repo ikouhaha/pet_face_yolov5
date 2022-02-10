f7b4f03ed13d42f67cb90a0c88bfc3fbd4c8952c
Dennis Au Yeung <ikouhaha888@gmail.com>
2022-02-10 22:14:28 +0800

    add serverlet and sample call method

---
+++ example_request.py
@@ -0,0 +1,33 @@
+"""Perform test request"""
+import pprint
+
+import requests
+from Util import *
+import cv2
+
+from utils.plots import Annotator, colors, save_one_box
+
+DETECTION_URL = "http://localhost:5000/v1/object-detection/yolov5s"
+TEST_IMAGE = "data\images\WhatsApp Image 2021-10-24 at 15.39.47.jpeg"
+
+image_data = open(TEST_IMAGE, "rb").read()
+
+response = requests.post(DETECTION_URL, files={"image": image_data}).json()
+
+img = cv2.imread(TEST_IMAGE)
+
+
+if(response["success"] == False):
+    exit
+
+#each pred
+for res in response["data"] :
+    print(res)
+    annotator = Annotator(img, line_width=3, example=str("cat"))
+    annotator.box_label([res["xmin"],res["ymin"],res["xmax"],res["ymax"]], res["name"] + " {:.2f}".format(res["confidence"]), color=colors(res["class"], True))
+    # Stream results
+    im0 = annotator.result()
+    cv2.imwrite("asdasd.jpg", im0)
+
+
+#pprint.pprint(response)
--- server.py
+++ server.py
@@ -3,12 +3,14 @@ Run a rest API exposing the yolov5s object detection model
 """
 import argparse
 import io
+from warnings import catch_warnings
 
 import torch
 from flask import Flask, request
 from PIL import Image
 import cv2
 import numpy as np
+import json
 
 app = Flask(__name__)
 
@@ -17,20 +19,26 @@ DETECTION_URL = "/v1/object-detection/yolov5s"
 
 @app.route(DETECTION_URL, methods=["POST"])
 def predict():
-    if not request.method == "POST":
-        return
+    
+    response = {}
+    try:
+        if not request.method == "POST":
+            return
 
-    if request.files.get("image"):
-        image_file = request.files["image"]
-        image_bytes = image_file.read()
+        if request.files.get("image"):
+            image_file = request.files["image"]
+            image_bytes = image_file.read()
+            img = Image.open(io.BytesIO(image_bytes))
+            results = model(img) 
+            response["data"] = json.loads(results.pandas().xyxy[0].to_json(orient="records"))
+            response["success"] = True
+        else:
+            response["success"] = False
+    except:
+       response["success"] = False
+    
 
-        img = Image.open(io.BytesIO(image_bytes))
-
-        results = model(img)  # reduce size=320 for faster inference
-
-        #results = model(cv2.cvtColor(cv2.imdecode(np.fromstring(image_bytes, np.uint8), cv2.IMREAD_COLOR), cv2.COLOR_RGB2BGR))
-
-        return results.pandas().xyxy[0].to_json(orient="records")
+    return response
 
 
 if __name__ == "__main__":
@@ -38,5 +46,5 @@ if __name__ == "__main__":
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
-    model = torch.hub.load("ikouhaha/pet_face_yolov5", "yolov5s", force_reload=False)  # force_reload to recache
+    model = torch.hub.load('ikouhaha/pet_face_yolov5','custom', path='runs/train/exp/weights/best.pt', force_reload=True)  # force_reload to recache
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
